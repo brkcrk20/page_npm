a5e7bee88e1534f8aa07f522709c7f041371bf1b
Berkay ÇIRAK <brkcrk20@gmail.com>
2026-01-08 09:10:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-259500215-f9fc6",
+  "appId": "1:314679110748:web:bedb825099b3407fba0a99",
+  "apiKey": "AIzaSyCLFaoNbey7eQTUl8U9xGGAi0EKwSqb48w",
+  "authDomain": "studio-259500215-f9fc6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "314679110748"
+};
