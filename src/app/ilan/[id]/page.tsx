e27e4f1502221adfceaa18d7865100ae7207185a
Berkay ÇIRAK <brkcrk20@gmail.com>
2026-01-10 06:03:34 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/ilan/[id]/page.tsx
+++ src/app/ilan/[id]/page.tsx
@@ -1,6 +1,7 @@
+
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
